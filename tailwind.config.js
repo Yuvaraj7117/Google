4caf20a2149b5a655c4bfe0e8b475ea57807f4e3
Yuvaraj7117 <danielguna494@gmail.com>
2024-08-18 08:20:44 +0530

    google

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor:{
+        bgColor:"red",
+      }
+    },
+  },
+  plugins: [],
+}
+
